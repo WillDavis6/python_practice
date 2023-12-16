c22bc3ed0d4287e1c5c8ec665c74f92a4ed41176
William <mailliwrd@gmail.com>
2023-12-16 15:27:50 -0700

    commit of py1 exercises

--- py1.py
+++ py1.py
@@ -1,5 +1,36 @@
-ducks = 3
-chickens = 33
+# num = 0
 
-if chickens >= ducks:
-    print ("not enough birda")
+# while num <= 100:
+#     if num == 50:
+#         break
+#     print(num)
+#     num += 10
+
+# print("all Done")
+
+# target = 4
+# guess = None
+
+# while guess != target:
+#     guess = input("Please enter a guess...")
+#     if guess == 'q' or guess == 'quit':
+#         break
+#     guess = int(guess)
+
+# print("ALL DONE")
+
+# things = ["banana", "licker", "pekachu"]
+# for items in things:
+#     print(items)
+
+def greet(person):
+    return f"hellow there, {person}"
+
+def divide(a,b):
+    if type(a) is int and type(b) is int:
+     return a/b
+    else:
+       return "you dummy"
+    
+def three_things(a,b,c):
+   print("hi")
