5900ad3d91cdbd2fe20b4b9532b8a28c3d63f0c2
William <mailliwrd@gmail.com>
2024-01-14 18:04:16 -0700

    jinja template manipulation

--- app.py
+++ app.py
@@ -6,3 +6,23 @@ app = Flask(__name__)
 @app.route('/hello')
 def say_hello():
     return "no time for the wicked"
+
+@app.route('/find-part')
+def get_part():
+    return """
+    <h1>search part data base</h1>
+    <form method="POST">
+        <input type="text" placeholder='part number' name='part-search'/>
+        <button>Search Data Base</button>
+    </form>
+        """
+
+@app.route('/find-part')
+def post_data():
+    return  f"<h1>search part data base</h1>\n\
+    <form method='POST'>\n\
+    <input type='text' placeholder='part number' name='part-search'/>\n\
+        <button>Search Data Base</button>\n\
+    </form>\n\
+    <p>{find_part(search_number)}"
+       
