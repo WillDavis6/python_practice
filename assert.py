38876a0047ab38510aaefd0595108ed889cf70a5
William <mailliwrd@gmail.com>
2024-01-30 17:47:04 -0700

    adding working doctest for ep

--- assert.py
+++ assert.py
@@ -1,4 +1,12 @@
 def adder(x, y):
+    '''
+    adds two numbers
+    >>> adder(3,4)
+    7
+    '''
+
+ 
+ 
     return x + y
 
-assert adder(2,5) == 8, "expect 7"
+#assert adder(2,5) == 8, "expect 7"
