1a8f50dcd604b05df1ff221bb6d8f064f00da393
William <mailliwrd@gmail.com>
2024-01-31 19:05:41 -0700

    integrated test example

--- test_file.py
+++ test_file.py
@@ -1,12 +1,25 @@
 from assert_file import adder
 from unittest import TestCase
 
+from app import app
+
 class AdditionTestCase(TestCase):
     """ examples of unittest case"""
 
     def test_adder(self):
 
         self.assertEqual(adder(2, 2), 4)
+        self.assertRaises(ValueError, adder, "f", "k")
+
+
+class ColorViewsTestCase(TestCase):
+    def test_color_form(self):
+        with app.test_client() as client:
+            res = client.get('/')
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code, 400)
+            self.assertIn('<h1>Color Form </h1>', html)
 
 
 
