5e2fb0a0237044d94df499ab6aeceeb7b16f4f38
William <mailliwrd@gmail.com>
2024-02-20 20:53:40 -0700

    sql commands

--- divide_algorythm.py
+++ divide_algorythm.py
@@ -6,3 +6,8 @@ def divide_conq(target, arr=[1,2,3,4,5,6,7,8,9])
     while mid_item != target:
         if target > mid_item:
             new_arr = arr[:mid_index + 1]
+
+
+NOT NULL
+UNIQUE
+DEFAULT
